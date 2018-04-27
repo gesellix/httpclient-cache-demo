07bb030646995b716793f4f28d4d196cbea95e4e
Tobias Gesellchen <tobias@gesellix.de>
2018-04-27 22:17:35 +0200

    remove irrelevant noise

--- src/main/java/de/gesellix/httpclientcachedemo/CouchDbConfig.java
+++ src/main/java/de/gesellix/httpclientcachedemo/CouchDbConfig.java
@@ -1,15 +1,11 @@
 package de.gesellix.httpclientcachedemo;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.joda.JodaModule;
 import org.ektorp.CouchDbConnector;
 import org.ektorp.CouchDbInstance;
 import org.ektorp.impl.ObjectMapperFactory;
 import org.ektorp.impl.StdCouchDbConnector;
 import org.ektorp.impl.StdCouchDbInstance;
+import org.ektorp.impl.StdObjectMapperFactory;
 import org.ektorp.spring.HttpClientFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -60,26 +56,6 @@ public class CouchDbConfig {
 
   @Bean
   ObjectMapperFactory objectMapperFactory() {
-    return new ObjectMapperFactory() {
-
-      @Override
-      public ObjectMapper createObjectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JodaModule());
-        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-
-        objectMapper.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, true);
-        objectMapper.setSerializationInclusion(JsonInclude.Include.ALWAYS);
-
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
-        return objectMapper;
-      }
-
-      @Override
-      public ObjectMapper createObjectMapper(CouchDbConnector connector) {
-        return createObjectMapper();
-      }
-    };
+    return new StdObjectMapperFactory();
   }
 }
